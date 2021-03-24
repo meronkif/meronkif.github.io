9aadcd77b7eb0e22deab17b4f7d4ca911baa9365
meron <meronkif@users.noreply.github.com>
2021-03-23 21:57:53 -0400

    Update app.js

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,19 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+function nameString(obj) {
+  if (obj === undefined) {
+    return "no object passed";
+  } else if (obj == null) {
+    return "null argument";
+  } else if (Object.keys(obj).length === 0) {
+    return "object has no properties";
+  } else {
+    let keys = [];
+    for (let index in obj) {
+      keys.push(index);
+    }
+    return keys.sort().join(".");
+  }
 }
+console.log(nameString({}));
